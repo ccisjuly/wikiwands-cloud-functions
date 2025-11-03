bfecc79664ee24fde26955a5a8411afb1d3a2dea
shawntsia <shawntsia@gmail>
2025-11-02 17:18:40 -0800

    feat: update

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -2,9 +2,10 @@ import * as admin from "firebase-admin";
 import {rcWebhook} from "./rcWebhook.js";
 import {onUserCreate} from "./onUserCreate.js";
 import {getUserProfile} from "./getUserProfile.js";
+import {updateUserProfile} from "./updateUserProfile.js";
 
 if (admin.apps.length === 0) {
   admin.initializeApp();
 }
 
-export {rcWebhook, onUserCreate, getUserProfile};
+export {rcWebhook, onUserCreate, getUserProfile, updateUserProfile};
